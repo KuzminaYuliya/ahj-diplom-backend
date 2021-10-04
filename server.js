3714ec992f70cb430185e5e03ef7adf1625eb359
KuzminaYuliya <kuzmina_h@mail.ru>
2021-10-04 22:56:22 +0500

    second commit for server

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-console */
 const http = require('http');
 const path = require('path');
 const Koa = require('koa');
@@ -17,6 +15,7 @@ const app = new Koa();
 const public = path.join(__dirname, '/public')
 app.use(koaStatic(public));
 
+// CORS
 app.use(async (ctx, next) => {
   const origin = ctx.request.get('Origin');
   if (!origin) {
@@ -49,6 +48,7 @@ app.use(async (ctx, next) => {
   }
 });
 
+
 app.use(koaBody({
   text: true,
   urlencoded: true,
@@ -60,54 +60,54 @@ const router = new Router();
 const server = http.createServer(app.callback())
 const wsServer = new WS.Server({ server });
 
-const arrayMessages = [];
+const arrMessges = [];
 
-let initMessage = false;
+let initMsg = false;
 
-router.get('/init', async (ctx, next) => {
-  if (!initMessage) {
-    initMessage = true;
-    const responseFetch = await fetch('https://ahj-diplom-backend.herokuapp.com/msg.json');
-    /* const responseFetch = await fetch('http://localhost:7070/msg.json'); */
-    const body = await responseFetch.text();
-    const arrayInitMessage = JSON.parse(body);
-    arrayMessages.push(...arrayInitMessage);
-    ctx.response.body = arrayMessages[0];
+router.get('/initmsg', async (ctx, next) => {
+  if (!initMsg) {
+    initMsg = true;
+    const resp = await fetch('https://ahj-diplom-backend.herokuapp.com/msg.json');
+    //const resp = await fetch('http://localhost:7070/msg.json');
+    const body = await resp.text();
+    const arrInitMsg = JSON.parse(body);
+    arrMessges.push(...arrInitMsg);
+    ctx.response.body = arrMessges[0];
   }
   ctx.response.body = 'ok';
 });
 
-router.get('/messagesArray', async (ctx, next) => {
-  console.log('get messages');
-  ctx.response.body = arrayMessages;
+router.get('/allmsg', async (ctx, next) => {
+  console.log('get index');
+  ctx.response.body = arrMessges;
 });
 
 router.get('/msg/:numb', async (ctx, next) => {
-  console.log('get number of message', ctx.params.numb);
-  const messagesLength = arrayMessages.length - ctx.params.numb;
-  const messagesStart = (messagesLength - 10) < 0 ? 0 : (messagesLength - 10);
-  const messagesReturn = arrayMessages.slice(messagesStart, messagesLength).reverse();
-  ctx.response.body = messagesReturn;
+  console.log('get numb', ctx.params.numb);
+  const endArr = arrMessges.length - ctx.params.numb;
+  const startArr = (endArr - 10) < 0 ? 0 : (endArr - 10);
+  const returnArr = arrMessges.slice(startArr, endArr).reverse();
+  ctx.response.body = returnArr;
 });
 
-router.post('/favoriteMessage', async (ctx, next) => {
-  const messageObject = JSON.parse(ctx.request.body);
-  const messageIndex = arrayMessages.findIndex((item) => JSON.parse(item).id === messageObject.id);
-  const parsedObj = JSON.parse(arrayMessages[itemIndex]);
-  parsedObj.favorite = messageObject.value;
-  arrayMessages[itemIndex] = JSON.stringify(parsedObj);
+router.post('/favorits', async (ctx, next) => {
+  const msgOb = JSON.parse(ctx.request.body);
+  const itemIndex = arrMessges.findIndex((item) => JSON.parse(item).id === msgOb.id);
+  const parsedObj = JSON.parse(arrMessges[itemIndex]);
+  parsedObj.favorit = msgOb.value;
+  arrMessges[itemIndex] = JSON.stringify(parsedObj);
   const obj = {
-    type: 'favoriteMessage',
-    id: messageObject.id,
-    value: messageObject.value,
+    type: 'change-favorit',
+    id: msgOb.id,
+    value: msgOb.value,
   };
   ctx.response.status = 204
 });
 
 wsServer.on('connection', (ws, req) => {
-  console.log('Server connected');
+  console.log('connection');
   ws.on('message', (msg) => {
-    arrayMessages.push(msg);
+    arrMessges.push(msg);
 
     [...wsServer.clients]
     .filter(o => {
@@ -117,13 +117,13 @@ wsServer.on('connection', (ws, req) => {
   });
 
   ws.on('close', (msg) => {
-    console.log('Server closed');
+    console.log('close');
     [...wsServer.clients]
 
     .filter(o => {
       return o.readyState === WS.OPEN;
     })
-    .forEach(o => o.send(JSON.stringify({type: 'delete user'})));
+    .forEach(o => o.send(JSON.stringify({type: 'del user'})));
     ws.close();
   });
 
